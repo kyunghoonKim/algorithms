fe40dc5a5a890b9a0129a7fa35c449b197fbd03d
kyunghunkim <kh.kim@purple.io>
2021-05-22 16:53:07 +0900

    10430

---
+++ acmicpc/10430/10430.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int a, b, c;
+
+	cin >> a >> b >> c;
+	cout << (a+b)%c << endl;
+	cout << ((a%c)+(b%c))%c << endl;
+	cout << a*b/c << endl;
+	cout << ((a%c)*(b%c))%c << endl;
+
+	return 0;
+}
